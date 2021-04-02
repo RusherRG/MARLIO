8559dfce25eeb30fa1e2779423a46d6e7dcce78c
RusherRG <rushang101@gmail.com>
2021-04-03 04:24:25 +0530

    Add embedding and one hot encoding to DQN

--- agents/agent_DQN.py
+++ agents/agent_DQN.py
@@ -1,16 +1,15 @@
 from collections import deque
 import tensorflow as tf
 from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.layers import Dense, Input, Embedding, Flatten, \
+    Reshape, Concatenate
 from tensorflow.keras.callbacks import TensorBoard
 import math
 import random
 import numpy as np
 
 
-TRAIN_EVERY = 16
-MINIBATCH_SIZE = 32
-DISCOUNT = 0.99
+DISCOUNT = 0.95
 
 
 class Strategy:
@@ -26,8 +25,8 @@ class Strategy:
 
         self.epsilon = self.config.epsilon
         self.discount = DISCOUNT
-        self.batch_size = MINIBATCH_SIZE
-        self.train_every = TRAIN_EVERY
+        self.batch_size = self.config.minibatch_size
+        self.train_every = self.config.train_every
         self.update_target_every = self.config.update_every
         self.epsilon_decay_value = self.config.epsilon_decay
 
@@ -46,28 +45,19 @@ class Strategy:
         x = Dense(n_outputs, name=f"{name}_output")(x)
         return x
 
-    def build_velocity_branch(self, x):
-        x = Dense(5, name="velocity_output")(x)
-        return x
-
-    def build_action_branch(self, x):
-        x = Dense(4, name="action_output")(x)
-        return x
-
-    def build_jump_branch(self, x):
-        x = Dense(2, name="jump_output")(x)
-        return x
-
-    def build_jump_down_branch(self, x):
-        x = Dense(2, name="jump_down_output")(x)
-        return x
-
     def create_model(self):
-        input_shape = (1, 132)
-        inputs = Input(shape=input_shape)
-        x = Dense(256, activation="relu")(inputs)
-        x = Dense(128, activation="relu")(inputs)
-        x = Dense(64, activation="relu")(inputs)
+        tiles = Input(shape=(7, 7), name="tiles")
+        tiles_out = Flatten()(tiles)
+        tiles_out = Embedding(5, 4)(tiles_out)
+        tiles_out = Reshape(target_shape=(196, ))(tiles_out)
+
+        state = Input(shape=(110,), name="state")
+
+        concat = Concatenate()([state, tiles_out])
+
+        x = Dense(256, activation="relu")(concat)
+        x = Dense(128, activation="relu")(x)
+        x = Dense(64, activation="relu")(x)
 
         aim = self.build_branch(x, 12, "aim")
         velocity = self.build_branch(x, 5, "velocity")
@@ -75,10 +65,12 @@ class Strategy:
         jump = self.build_branch(x, 2, "jump")
         jump_down = self.build_branch(x, 2, "jump_down")
 
-        model = Model(inputs=inputs, outputs=[
+        model = Model(inputs=[state, tiles], outputs=[
                       aim, velocity, action, jump, jump_down])
         model.compile(optimizer='Adam',
                       loss='MSE', metrics=['accuracy'])
+
+        self.logger.info(model.summary())
         return model
 
     def load_model(self, model_path):
@@ -88,17 +80,23 @@ class Strategy:
     def preprocess(self, state):
         state = state[0]
         input_state = [state["player_score"], state["opp_score"]]
-        
-        if len(state["units"]) == 0 or len(state["opp_units"]) == 0:
-            input_state.extend([0]*130)
-            return np.array(input_state).reshape(1, -1)
 
-        input_state.extend(state["units"][0][1:-1])
-        input_state.extend(state["opp_units"][0][1:-1])
-        
-        for tiles in state["units"][0][-1]:
-            input_state.extend(tiles)
-        
+        if len(state["units"]) == 0 or len(state["opp_units"]) == 0:
+            return [np.array([0]*110), np.array([[0]*7]*7)]
+
+        for unit in state["units"] + state["opp_units"]:
+            for i in range(1, len(unit)-1):
+                if i < 7:
+                    input_state.append(unit[i])
+                elif i == 7:
+                    input_state.extend(
+                        tf.one_hot(unit[i], 3,
+                                   on_value=1.0, off_value=0.0).numpy().tolist())
+                else:
+                    input_state.extend(
+                        tf.one_hot(unit[i], 2,
+                                   on_value=1.0, off_value=0.0).numpy().tolist())
+
         unit_x = state["units"][0][1]
         unit_y = state["units"][0][2]
 
@@ -118,30 +116,38 @@ class Strategy:
             if dist < min_dist:
                 min_dist = dist
                 closest_mine = mine
+        closest_mine = closest_mine[:2] + \
+            tf.one_hot(closest_mine[2], 4,
+                       on_value=1.0, off_value=0.0).numpy().tolist() + \
+            closest_mine[3:]
         input_state.extend(closest_mine)
 
         for loot in state["loot_boxes"][:10]:
-            input_state.extend(loot)
-
+            input_state.extend(loot[:2] +
+                               tf.one_hot(loot[2], 3,
+                                          on_value=1.0, off_value=0.0)
+                               .numpy().tolist() +
+                               loot[3:])
         prev_len = len(input_state)
-        for _ in range(prev_len, 132):
+        for i in range(prev_len, 110):
             input_state.append(0)
-        input_state = input_state[:132]
 
         for i in range(len(input_state)):
             if input_state[i] is None or type(input_state[i]) == dict:
                 input_state[i] = 0
 
-        return np.array(input_state).reshape(1, -1)
+        return [np.array(input_state), np.array(state["units"][0][-1])]
 
     def act(self, state):
-        self.logger.debug("State")
-        self.logger.debug(state[0])
+        # self.logger.debug("State")
+        # self.logger.debug(state[0])
         input_state = self.preprocess(state)
-        self.logger.debug(input_state)
-        self.logger.debug(input_state.shape)
+        # self.logger.debug(input_state)
 
-        current_q_values = self.model.predict(np.array([input_state]))
+        current_q_values = self.model.predict([
+            input_state[0].reshape(1, -1),
+            input_state[1].reshape(1, 7, 7)
+        ])
         self.logger.debug("Action")
         self.logger.debug(current_q_values)
         if np.random.random() > self.epsilon:
@@ -165,30 +171,41 @@ class Strategy:
             return
 
         minibatch = random.sample(self.memory, self.batch_size)
-        X, Y = [], [[] for i in range(5)]
+        X_state, X_tiles, Y = [], [], [[] for i in range(5)]
 
         for cur_state, action, reward, new_state, done in minibatch:
             if not done:
                 # get the future q value
-                future_q = self.target_model.predict(np.array([new_state]))
+                future_q = self.target_model.predict([
+                    new_state[0].reshape(1, -1),
+                    new_state[1].reshape(1, 7, 7)
+                ])
                 new_q = [reward + self.discount*np.max(future_q[i][0])
                          for i in range(len(action))]
             else:
                 new_q = [reward for i in range(len(action))]
+            
             # get current q-value and update it with new q-value
-            cur_q = self.model.predict(np.array([cur_state]))
+            cur_q = self.model.predict([
+                cur_state[0].reshape(1, -1),
+                cur_state[1].reshape(1, 7, 7)
+            ])
             for i in range(len(action)):
-                cur_q[i][0] = cur_q[i][0][0]
+                cur_q[i] = cur_q[i][0]
             for i in range(len(action)):
-                cur_q[i][0][0][action[i]] = new_q[i]
+                cur_q[i][action[i]] = new_q[i]
+            
+            X_state.append(cur_state[0])
+            X_tiles.append(cur_state[1])
+            for i in range(len(action)):
+                Y[i].append(cur_q[i])
 
-            X.append(cur_state)
-            for i in range(len(action)):
-                Y[i].append(cur_q[i][0])
         for i in range(len(action)):
             Y[i] = np.array(Y[i])
 
-        self.model.fit(np.array(X), Y,
+        self.model.fit({"state": np.array(X_state),
+                        "tiles": np.array(X_tiles)},
+                       Y,
                        batch_size=self.batch_size,
                        epochs=self.epoch_count+self.config.epochs,
                        initial_epoch=self.epoch_count)
